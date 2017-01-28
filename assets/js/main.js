19790bb2fad0e8c29f626a8f4f6df90e1e5f3a04
Harry Park <harry@harrypark.io>
2017-01-28 18:00:26 +1100

    meme api implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,34 @@
+getMeme();
+
 var currentMeme = 0;
 
 var audio = document.getElementById('audio');
 
-$left = $('#left');
-
-$left.css({"cursor":"pointer"});
-$left.click(function() {
-	audio.play();
-	prevMeme();
-	getMeme();
-});
-
-$right = $('#right');
-
-$right.css({"cursor":"pointer"});
-$right.click(function() {
-	audio.play();
-	nextMeme();
-	getMeme();
-});
-
-$(function() {
-	$('a[href^="#static"]').bind('click', function(event) {
-		event.preventDefault();
-	});
-});
-
-function nextMeme() {
-	currentMeme += 1;
-}
-
-function prevMeme() {
-	if (currentMeme > 0) {
-		currentMeme -= 1;
-	}
-}
-
 function getMeme() {
-	console.log(currentMeme);
-	// return url;
+	var request = new XMLHttpRequest();
+	var httpurl = 'https://api.imgur.com/3/g/memes/viral/' + Math.floor((Math.random() * 30) + 1);
+	request.open("GET", httpurl, true);
+	request.setRequestHeader('Authorization', 'Client-ID 2eeac4357a889f9')
+	request.send();
+
+	request.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var data = JSON.parse(this.responseText);
+			var counter = 0;
+			$('#mememeup').on('click', function() {
+				if (counter > 55) {
+					counter = 0;
+				} else {
+					var id = data.data[counter].link;
+					var width = data.data[counter].width;
+					$('img.meme').attr('src', id);
+					counter++;
+				}
+			});
+		}
+	};
 }
 
-
-
-
-
-
-
-
-
-var request = new XMLHttpRequest();
-var httpurl = '';
-request.open("GET", httpurl, true);
-
-request.onreadystatechange = function() {
-	if (this.readyState == 4 && this.status == 200) {
-		var data = JSON.parse(this.responseText);
-	}
-};
+$('#mememeup').on('click', function() {
+	audio.play();
+});
--- preprocessor/assets/js/main.js
+++ preprocessor/assets/js/main.js
@@ -1,40 +1,8 @@
+getMeme();
+
 var currentMeme = 0;
 
 var audio = document.getElementById('audio');
-
-$left = $('#left');
-
-$left.css({"cursor":"pointer"});
-$left.click(function() {
-	audio.play();
-	prevMeme();
-	getMeme();
-});
-
-$right = $('#right');
-
-$right.css({"cursor":"pointer"});
-$right.click(function() {
-	audio.play();
-	nextMeme();
-	getMeme();
-});
-
-$(function() {
-	$('a[href^="#static"]').bind('click', function(event) {
-		event.preventDefault();
-	});
-});
-
-function nextMeme() {
-	currentMeme += 1;
-}
-
-function prevMeme() {
-	if (currentMeme > 0) {
-		currentMeme -= 1;
-	}
-}
 
 function getMeme() {
 	var request = new XMLHttpRequest();
@@ -45,8 +13,22 @@ function getMeme() {
 
 	request.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			var data = JSON.parse(this.responseText).data[0].link;
-			console.log(data);
+			var data = JSON.parse(this.responseText);
+			var counter = 0;
+			$('#mememeup').on('click', function() {
+				if (counter > 55) {
+					counter = 0;
+				} else {
+					var id = data.data[counter].link;
+					var width = data.data[counter].width;
+					$('img.meme').attr('src', id);
+					counter++;
+				}
+			});
 		}
 	};
 }
+
+$('#mememeup').on('click', function() {
+	audio.play();
+});
